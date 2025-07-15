e0eec2b5ef6ccff9cb385467177eabff7ef8e045
rekky1aws <killikomfr@gmail.com>
2025-07-16 01:15:47 +0200

    Bash script to copy all files to home dir

---
+++ export_to_home.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# SH Init files
+cp ./.bashrc ~/.bashrc
+cp ./.zshrc ~/.zshrc
+
+# Kitty
+cp ./.config/kitty/kitty.conf  ~/.config/kitty/kitty.conf
+
+# Neovim
+cp ./.config/nvim/init.lua ~/.config/nvim/init.lua
+cp ./.config/nvim/init.vim.bak ~/.config/nvim/init.vim.bak
