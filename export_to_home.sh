d142fda42a021655b7981446b5f2eba2c6669574
rekky1aws <killikomfr@gmail.com>
2025-08-06 21:52:23 +0200

    Export script update
    Updated `export_to_home.sh` to export all the new configuration files

--- export_to_home.sh
+++ export_to_home.sh
@@ -1,12 +1,8 @@
 #! /bin/bash
 
-# SH Init files
+# Shell Init files
 cp ./.bashrc ~/.bashrc
 cp ./.zshrc ~/.zshrc
 
-# Kitty
-cp ./.config/kitty/kitty.conf  ~/.config/kitty/kitty.conf
-
-# Neovim
-cp ./.config/nvim/init.lua ~/.config/nvim/init.lua
-cp ./.config/nvim/init.vim.bak ~/.config/nvim/init.vim.bak
+# .config folder
+cp -r ./.config/* ~/.config
